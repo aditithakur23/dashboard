779767d25d2ff8cc9495534bab5441e96b30a8b4
aditithakur23 <85165488+aditithakur23@users.noreply.github.com>
2021-09-29 17:02:11 +0530

    Update Routes.jsx

--- components/Routes.jsx
+++ components/Routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import {Route , Switch} from 'react-router-dom'
-
+import Products from '../pages/Products'
 import Dashboard from '../pages/Dashboard'
 import Customers from '../pages/Customers'
 
@@ -10,6 +10,7 @@ const Routes = () => {
         <Switch>
             <Route path='/' exact component={Dashboard}/>
             <Route path='/customers' component={Customers}/>
+            <Route path='/products' component={Products}/>
         </Switch>
     )
 }
